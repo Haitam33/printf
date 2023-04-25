52fab92a38e6fc7b37d9521ad9d1a346159258fb
Haitam33 <haitam.mastaki@gmail.com>
2023-04-25 14:47:15 -0700

    push 1

--- main.h
+++ main.h
@@ -2,5 +2,8 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+char *str_rev (char *str);
+char * _itoau(long int i, char *strout, int base);
+char * _itoa(long int i, char *strout, int base);
 
 #endif /* MAIN_H */
