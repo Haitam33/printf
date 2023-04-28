9c3878a948d59a04eea8579dd8253868c8b69d45
Haitam33 <haitam.mastaki@gmail.com>
2023-04-28 12:45:45 -0700

    2. With a face like mine, I do better in print

--- _printf.c
+++ _printf.c
@@ -13,57 +13,93 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			switch (format[i])
-			{
-				/* Convert char */
-				case 'c':
-					{
-						buff[j] = (char)va_arg(vl, long int);
-						j++;
-						break;
-					}
-					/* Convert decimal */
-				case 'd':
-					{
-						_itoa((int)va_arg(vl, long int), tmp, 10);
-						strcpy(&buff[j], tmp);
-						j += strlen(tmp);
-						break;
-					}
-					/* Convert integer*/
-				case 'i':
-					{
-						_itoa((int)va_arg(vl, long int), tmp, 10);
-						strcpy(&buff[j], tmp);
-						j += strlen(tmp);
-						break;
-					}
-					/* case string */
-				case 's':
-					{
-						  str_arg = va_arg(vl, char*);
-						  strcpy(&buff[j], str_arg);
-						  j += strlen(str_arg);
-					break;
-					}
-					/* Convert to binaire */
-				case 'b':
-					{
-						_itoa((int)va_arg(vl, long int), tmp, 2);
-						strcpy(&buff[j], tmp);
-						j += strlen(tmp);
+		switch (str[i]) {
+					/* Convert char */
+        case 'c': {
+          buff[j] = (char)va_arg( vl,long int );
+          j++;
+          break;
+        }
+        /* Convert decimal */
+        case 'd': {
+          _itoa((int)va_arg( vl,long int ), tmp, 10);
+          strcpy(&buff[j], tmp);
+          j += strlen(tmp);
+          break;
+        }
+        /* unsigned integer */
+        case 'u': {
+          _itoa((unsigned int)va_arg( vl,long int ), tmp, 10);
+          strcpy(&buff[j], tmp);
+          j += strlen(tmp);
+          break;
+        }
+		/* Convert integer*/
+        case 'i': {
+          _itoa((int)va_arg( vl,long int ), tmp, 10);
+          strcpy(&buff[j], tmp);
+          j += strlen(tmp);
+          break;
+        }
+        /* Convert hex Lowercase*/
+        case 'x': {
+          _itoa(va_arg( vl,long int ), tmp, 16);
+          strcpy(&buff[j], tmp);
+          j += strlen(tmp);
+          break;
+        }
+         /* Convert hexadecimal Uppercase */
+        case 'X': {
+          _itoau(va_arg( vl,long int ), tmp, 16);
+          strcpy(&buff[j], tmp);
+          j += strlen(tmp);
+          break;
+        }
+        /* Convert octal */
+        case 'o': {
+          _itoa(va_arg( vl,long int ), tmp, 8);
+          strcpy(&buff[j], tmp);
+          j += strlen(tmp);
+          break;
+        }
+        /* case string */
+        case 's': {
+          str_arg = va_arg( vl, char* );
+          strcpy(&buff[j], str_arg);
+          j += strlen(str_arg);
+          break;
+        }
+		/* case % */
+        case '%': {
+        char *str;
 
-					  /* case % */
-				case '%':
-					{
-						  char *str;
+        str = "%";
+        strcpy(&buff[j], str);
+        j++;
+          break;
+        }
+        /* case adresse */
+        case 'p': {
+        char a[] = "0x";
+        buff[j] = a[0];
+        j++;
+        buff[j] = a[1];
+        j++;
+        _itoa(va_arg( vl,long int ), tmp, 16);
+          strcpy(&buff[j], tmp);
+          j += strlen(tmp);
+          break;
+        }
+        /* case adresse */
+        case 'r': {
+        char *str;
 
-						  str = "%";
-						  strcpy(&buff[j], str);
-						  j++;
-					break;
-					}
-			}
+        str = "%r";
+        buff[j] = str[0];
+        j++;
+        buff[j] = str[1];
+        j++;
+          break;
 		}
 		else
 		{
